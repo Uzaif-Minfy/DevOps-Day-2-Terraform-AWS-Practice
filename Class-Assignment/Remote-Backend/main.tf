0fd7e3ad0518cb926e9118a7d71712e9fe53576c
Uzaif-Minfy <uzaif.ali@minfytech.com>
2025-06-12 20:11:06 +0530

    add remote backend

---
+++ Class-Assignment/Remote-Backend/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "minfy-training-uzaif-s3-2025"
+    key            = "global/s3/terraform.tfstate" 
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
